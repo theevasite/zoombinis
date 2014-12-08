75dfb0da965f5cbb862f7f46b026d7ad8babcb49
Eva Perello <eva@theevasite.com>
2014-12-08 17:19:06 -0500

    Initial_MUSEcode

---
+++ assets/videogame/videogame_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x3='5.0.1.386',x1='5.0.1',x7='rgba(255,255,255,1)',x2='5.0.0',i='none';var g5='videogame.ogv',g6='videogame.mp4',g4='videogame.webm';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'videogame',t:'video',tag:'video',r:['0px','0px','1440px','820px','auto','auto'],ap:'autoplay',lp:'loop',sr:[vid+g4,vid+g5,vid+g6],pr:'auto'}],style:{'${Stage}':{isStage:true,r:['null','null','1440px','820px','auto','auto'],overflow:'hidden',f:[x7]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-25276752");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-25276752");
---
+++ assets/videopromo/videopromo_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x5='rgba(255,255,255,1)',x2='5.0.0',i='none',x3='5.0.1.386',x1='5.0.1';var g4='videopromo.mp4';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'videopromo',t:'video',tag:'video',r:['0','0','1440','820','auto','auto'],ap:'autoplay',lp:'loop',sr:[vid+g4],pr:'auto'}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'1440px','820px'],overflow:'hidden',f:[x5]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-13433789");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-13433789");
